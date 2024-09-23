b9a3ba6ba5d43d7fc5a0b2c88a156c44e6e9b135
alejo5600 <alejo5600@gmail.com>
2024-09-23 08:11:54 -0500

    indentation

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -1,18 +1,18 @@
 <template>
     <v-data-table
     :headers="headers"
-  :items="tasks"
-item-value="id"
-  select-strategy="single"
-  show-select
-  :model-value="[localIdTaskSelected]"
-  @update:model-value="(val) => localIdTaskSelected = val[0]"
+    :items="tasks"
+    item-value="id"
+    select-strategy="single"
+    show-select
+    :model-value="[localIdTaskSelected]"
+    @update:model-value="(val) => localIdTaskSelected = val[0]"
     >
       <template v-slot:item.select="{ item }">
         <v-checkbox
-      :model-value="localIdTaskSelected"
+        :model-value="localIdTaskSelected"
         @update:model-value="updateIdTaskSelected(item.id)"
-      :value="item.id"
+        :value="item.id"
     ></v-checkbox>
       </template>
       <template v-slot:item.status="{ item }">
